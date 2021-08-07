ad08147c0b750048e77c8360fd46027eda3d84f1
LeoScripts <leandroguitarjesus89@gmail.com>
2021-08-07 13:27:40 -0300

    adiçao de alguns comentarios

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+//selecionado o main e adicionando na varialvel main
 const main = document.querySelector('main')
 
+//array com imagens e textos que seram usados 
 const expressaoHumana = [
     {img:'./img/andar de bicicleta.jpg', texto: 'andar de bicicleta'},
     {img:'./img/brincar-de-carrinho.jpeg', texto: 'bricar de carrinho'},
@@ -12,14 +14,17 @@ const expressaoHumana = [
 ]
 
 const criarExpressao = ({ img, texto }) => {
+    //criando a div la no html
     const div = document.createElement('div')
-
+    //adicionando a classe = expressao-box
     div.classList.add('expressao-box')
+    //criando as tags img e p que estarao na nossa div
     div.innerHTML = `
         <img src="${img}" alt="${texto}">
         <p class="info">${texto}</p>
     `
-
+    // adicionando a div aqui criada como ultimo filho da main do html
+    // que que esta vazio
     main.appendChild(div)
 }
 
