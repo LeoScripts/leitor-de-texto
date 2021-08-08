d0805f919e8f7822d95c57bd49a1d10a4b5cc091
LeoScripts <leandroguitarjesus89@gmail.com>
2021-08-08 00:31:09 -0300

    funçoes de adcionadas, faltando algumas coisas antes da refatoraçao

--- app.js
+++ app.js
@@ -13,26 +13,58 @@ const saiDivCaixaTexto = document.querySelector('.sair')
 //selecionando o select pra colocarmos os options que criamos aqui
 const idiomas = document.querySelector('select')
 
+//selecinando o botao com o id = ler
+const ler = document.querySelector('#ler')
+
+//selecionando textarea pra receber o texto digitado nele
+const textArea = document.querySelector('textarea')
+
+
 //array com imagens e textos que seram usados 
 const expressaoHumana = [
-    {img:'./img/andar de bicicleta.jpg', texto: 'andar de bicicleta'},
-    {img:'./img/oração.jpg', texto: 'vamos orar'},
-    {img:'./img/dormir.jpg', texto: 'hora dormir'},
-    {img:'./img/banho.jpg', texto: 'toma banho'},
-    {img:'./img/escovar dente.jpg', texto: 'escovar os dentes'},
-    {img:'./img/estudar.jpg', texto: 'estudar'}
+    {img:'./img/andar de bicicleta.jpg', text: 'andar de bicicleta'},
+    {img:'./img/oração.jpg', text: 'vamos orar'},
+    {img:'./img/dormir.jpg', text: 'hora de dormir'},
+    {img:'./img/banho.jpg', text: 'tomar banho'},
+    {img:'./img/escovar dente.jpg', text: 'escovar os dentes'},
+    {img:'./img/estudar.jpg', text: 'estudar'}
 ]
 
-const criarExpressao = ({ img, texto }) => {
+const textoDigitado = new SpeechSynthesisUtterance()
+const inserindoTexto = text => {
+    textoDigitado.text = text
+}
+
+const somDoTexto = () => {
+    speechSynthesis.speak(textoDigitado)
+}
+
+const setVoice = event => {
+    const selecaoDeVoz = voices.find(voice => voice.name === event.target.value)
+    textoDigitado.voice = selecaoDeVoz
+}
+
+const criarExpressao = ({ img, text }) => {
     //criando a div la no html
     const div = document.createElement('div')
     //adicionando a classe = expressao-box
     div.classList.add('expressao-box')
     //criando as tags img e p que estarao na nossa div
     div.innerHTML = `
-        <img src="${img}" alt="${texto}">
-        <p class="info">${texto}</p>
+        <img src="${img}" alt="${text}">
+        <p class="info">${text}</p>
     `
+
+    div.addEventListener('click', () => {
+        inserindoTexto(text)
+        somDoTexto()
+
+        div.classList.add('active')
+        setTimeout(()=>{
+            div.classList.remove('active')
+        },1000)
+    })
+
     // adicionando a div aqui criada como ultimo filho da main do html
     // que que esta vazio
     main.appendChild(div)
@@ -44,15 +76,21 @@ expressaoHumana.forEach(criarExpressao);
 
 let voices = []
 
-//adicionando pacote colecao de vozes
+//adicionando pacote colecao idioma de vozes
 speechSynthesis.addEventListener('voiceschanged', () => {
     voices = speechSynthesis.getVoices()
 
     voices.forEach(({ name, lang }) => {
+        //criando o elemento option
         const option = document.createElement('option')
 
         option.value = name
         option.textContent = `${lang} | ${name}`
+
+
+        
+
+        //inserindo as options na select
         idiomas.appendChild(option)   
     })
 })
@@ -69,4 +107,11 @@ botaoTexto.addEventListener('click',()=>{
 saiDivCaixaTexto.addEventListener('click', ()=>{
     //desativando a caixa texarea atravez da classe show
     divCaixaTexto.classList.remove('show')
+})
+
+idiomas.addEventListener('change', setVoice)
+
+ler.addEventListener('click', ()=>{
+    inserindoTexto(textArea.value)
+    somDoTexto()
 })
